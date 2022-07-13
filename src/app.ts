e9678131a82d69b6ce0dfb974783e8e8a60501e2
Bolaji Ayodeji <sirbeejay1@gmail.com>
2022-07-13 18:46:41 +0100

    refactor: 💡 Fix gateway response issues

--- src/app.ts
+++ src/app.ts
@@ -9,9 +9,9 @@ const app = require("express")();
 const port = process.env.PORT || 9000;
 
 interface tweetUsers {
-  id: string;
-  username: string;
-  name: string;
+  id: string | undefined;
+  username: string | undefined;
+  name: string | undefined;
   email: string;
   tweet: string;
   created_at: string | undefined;
@@ -32,61 +32,72 @@ app.post("/authorize", (req: Request, res: Response) => {
   const encode = hash.toString(CryptoJS.enc.Base64);
 
   if (req.method === "POST" && signature === encode) {
-    const payload = req.body;
+    const payload = JSON.parse(req.rawBody);
 
-    payload.included.map((order: any) => {
-      const startTime = order.attributes.created_at;
+    payload.included.slice(0, 1).map((order: any) => {
+      const startTime = order.attributes.updated_at;
       const endTime = new Date(
-        new Date(startTime).getTime() + 60000
+        new Date(startTime).getTime() + 1800000
       ).toISOString();
       const orderNumber = order.attributes.number;
       const customerEmail = order.attributes.customer_email;
       const amountCents = order.attributes.subtotal_taxable_amount_cents;
       const transactionToken = order.attributes.token;
+      // console.log(startTime, endTime, orderNumber, customerEmail, amountCents);
 
       getTweets(startTime, endTime).then((data) => {
-        const tweets = data.tweets;
-        const users = data.users;
+        if (data.tweets.length !== 0) {
+          const tweets = data.tweets;
+          const users = data.users;
+          let customerData: tweetUsers;
 
-        tweets.forEach((tweet) => {
-          if (tweet.text.includes(orderNumber.toString())) {
-            const customerData: tweetUsers[] = [];
-            const user = users.find((u) => u.id === tweet.author_id);
-            if (user) {
-              customerData.push({
-                id: user.id,
-                username: user.username,
-                name: user.name,
+          tweets.map((tweet) => {
+            if (tweet.text.includes(orderNumber)) {
+              customerData = {
+                id: tweet.author_id,
+                username: users.find((u) => u.id === tweet.author_id)?.username,
+                name: users.find((u) => u.id === tweet.author_id)?.name,
                 email: customerEmail,
                 tweet: tweet.text,
                 created_at: tweet.created_at
+              };
+              res.status(200).json({
+                success: true,
+                data: {
+                  transaction_token: transactionToken,
+                  amount_cents: amountCents,
+                  metadata: {
+                    ...customerData
+                  }
+                }
+              });
+            } else {
+              res.status(422).json({
+                success: false,
+                data: {
+                  transaction_token: transactionToken,
+                  amount_cents: amountCents,
+                  error: {
+                    code: "404",
+                    message: `Order number: ${orderNumber} not found in tweets`
+                  }
+                }
               });
             }
-            console.log(customerData);
-            res.status(200).json({
-              success: true,
-              data: {
-                transaction_token: transactionToken,
-                amount_cents: amountCents,
-                metadata: {
-                  ...customerData
-                }
+          });
+        } else {
+          res.status(422).json({
+            success: false,
+            data: {
+              transaction_token: transactionToken,
+              amount_cents: amountCents,
+              error: {
+                code: "404",
+                message: `Tweet with order number: ${orderNumber} not found.`
               }
-            });
-          } else {
-            res.status(422).json({
-              success: false,
-              data: {
-                transaction_token: transactionToken,
-                amount_cents: amountCents,
-                error: {
-                  code: "404",
-                  message: `Order number: ${orderNumber} not found in tweets`
-                }
-              }
-            });
-          }
-        });
+            }
+          });
+        }
       });
     });
   } else {
